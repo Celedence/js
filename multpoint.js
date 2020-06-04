ff55fc9c52f3bae8000712b59ce9ddde0ff06bf5
brian <brianbb174@protonmail.com>
2020-06-03 17:55:58 -0700

    add multipoint

---
+++ multpoint.js
@@ -0,0 +1,10 @@
+
+function countUnique(arr){
+    var i = 0;
+    for(var j = 1; j < arr.length; j++){
+        if(arr[i] !== arr[j]){
+            i++;
+            arr[i] = arr[j]
+        }
+    }
+}
