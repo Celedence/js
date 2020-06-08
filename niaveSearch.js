159f9bf8b4e830c44a03a85cde2b95b42a326aff
brian <brianbb174@protonmail.com>
2020-06-08 10:47:05 -0700

    add more logic

--- niaveSearch.js
+++ niaveSearch.js
@@ -1,9 +1,18 @@
 function niaveSearch(long, short){
+    var count = 0;
     for(var i = 0; i < long.length; i++){
         for(var j = 0; j < short.length; j++){
-            console.log(long[i],short[j])
+            console.log(short[j],long[i+j]);
+            if(short[j] !== long[i+j]){
+                break;           
+            }
+            if(j === short.length -1){
+                console.log("found one!");
+                count++;
+            }
         }
     }
+    return count;
 }
 
 niaveSearch("lorie loled", "lol")
