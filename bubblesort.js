d8be0bbe3ab7bbf6f6eef3f49a6147dd85b36e3a
brian <brianbb174@protonmail.com>
2020-06-10 09:38:46 -0700

    bubblesort

---
+++ bubblesort.js
@@ -0,0 +1,15 @@
+function bubbleSort(arr){
+    for(var i = 0; i < arr.length; i++){
+        for(var j = 0; j < arr.length; j++){
+            if(arr[j] > arr[j + 1]){
+                //swap
+                var temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+// the niave method
+bubbleSort([23,45,22,1]);
