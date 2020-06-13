8a183a207d63967bab602a0c45f118bb64f270c0
brian <brianbb174@protonmail.com>
2020-06-13 14:16:19 -0700

    add finale merge

--- merge.js
+++ merge.js
@@ -19,10 +19,16 @@ function merge(arr1, arr2){
         results.push(arr2[j])
         j++;
     }
+    console.log(results);
     return results;
 }
 
 function mergeSort(arr){
     if(arr.length <= 1) return arr;
-    //mergeSort()
+    let mid = Math.floor(arr.length / 2);
+    let left = mergeSort(arr.slice(0,mid));
+    let right = mergeSort(arr.slice(mid));
+    return merge(left,right);
 }
+
+mergeSort([10,24,76,73,72,1,9])
