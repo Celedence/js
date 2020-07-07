7b36b3d3c6f9445909157f349664f8c39138ba23
brian <brianbb174@protonmail.com>
2020-07-06 19:42:36 -0700

    initial class

---
+++ bsearchtree.js
@@ -0,0 +1,14 @@
+
+class Node {
+    constructor() {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+}
