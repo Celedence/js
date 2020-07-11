b3761e3c8501bda3ed4862687ea63b8747f0a57c
brian <brianbb174@protonmail.com>
2020-07-11 13:25:45 -0700

    appended push

--- bsearchtree.js
+++ bsearchtree.js
@@ -72,7 +72,7 @@ class BinarySearchTree {
         var data = [];
         var current = this.root;
         function traverse(node){
-            data.push(node);
+            data.push(node.value);
             if(node.left) traverse(node.left);
             if(node.right) traverse(node.right);
         }
