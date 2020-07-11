660f542459ea036122ce502d6d66f415e1b2e158
brian <brianbb174@protonmail.com>
2020-07-10 19:05:46 -0700

    finale

--- bsearchtree.js
+++ bsearchtree.js
@@ -1,6 +1,6 @@
 
 class Node {
-    constructor() {
+    constructor(value) {
         this.value = value;
         this.left = null;
         this.right = null;
@@ -53,5 +53,19 @@ class BinarySearchTree {
         }
         if(!found) return undefined;
         return current;
+    }
+    BFS(){
+        var node = this.root;
+        var data = [];
+        var queue = [];
+
+        queue.push(node);
+        while(queue.length){
+            node = queue.shift();
+            data.push(node.value);
+            if(node.left) queue.push(node.left);
+            if(node.right) queue.push(node.right);
+        }
+        return data;
     }
 }
