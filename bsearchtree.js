7e01b499e8b5db8b82c69d19b9a1b9b1ab4a00f2
brian <brianbb174@protonmail.com>
2020-07-13 10:09:24 -0700

    add order

--- bsearchtree.js
+++ bsearchtree.js
@@ -80,4 +80,26 @@ class BinarySearchTree {
         return data;
 
     }
+    DFSPO(){
+        var data = [];
+        var current = this.root;
+        function traverse(node){
+            if(node.left) traverse(node.left);
+            if(node.right) traverse(node.right);
+            data.push(node.value);
+        }
+        traverse(current);
+        return data;
+    }
+    DFSInOrder(){
+        var data = [];
+        var current = this.root;
+        function traverse(node){
+            if(node.left) traverse(node.left);
+            data.push(node.value);
+            if(node.right) traverse(node.right);
+        }
+        traverse(current);
+        return data;
+    }
 }
