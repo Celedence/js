d641fe4c40c9a62aa99ebb135e451c0a556e7e4e
brian <brianbb174@protonmail.com>
2020-07-17 08:25:34 -0700

    add edge case

--- heaps.js
+++ heaps.js
@@ -23,8 +23,10 @@ class MaxBinaryHeap{
         //edge case will return to this
         const max = this.values[0];
         const end = this.values.pop();
-        this.values[0] = end;
-        this.sinkDown();
+        if(this.values.length > 0){
+            this.values[0] = end;
+            this.sinkDown();
+    }
         return max;
     }
     sinkDown(){
