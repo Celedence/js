2ddae7add51cae662d9032520d58f73c486b1592
brian <brianbb174@protonmail.com>
2020-07-16 19:16:29 -0700

    initial heap

---
+++ heaps.js
@@ -0,0 +1,24 @@
+
+class MaxBinaryHeap{
+    constructor(){
+        this.values = [];
+    }
+    insert(value){
+        this.values.push(value);
+        this.bubbleUp();
+    }
+    bubbleUp(){
+        let idx = this.values.length -1;
+        const element = this.values[idx];
+        while(idx > 0){
+            let parentIdx = Math.floor((idx -1)/2);
+            let parent = this.values[parentIdx];
+            if(element <= parent) break;
+            this.values[parentIdx] = element;
+            this.values[idx] = parent;
+            idx = parentIdx;
+        }
+    }
+}
+let heap = new MaxBinaryHeap();
+heap.insert(55);
