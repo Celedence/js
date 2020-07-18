b9bb32a359cb2c2fe8ef8c9ed74ea5c37f368342
brian <brianbb174@protonmail.com>
2020-07-18 10:05:00 -0700

    add better hash

--- hash.js
+++ hash.js
@@ -1,16 +1,25 @@
 
 //not so great hash
 
-function hash(key, arrayLen) {
-    let total = 0;
-    for (let i = 0; i < key.length; i++){
-        let char = key[i];
-        let value = char.charCodeAt(0) - 96;
-        total = (total + value) % arrayLen;
-    }
-    return total;
+// function hash(key, arrayLen) {
+//     let total = 0;
+//     for (let i = 0; i < key.length; i++){
+//         let char = key[i];
+//         let value = char.charCodeAt(0) - 96;
+//         total = (total + value) % arrayLen;
+//     }
+//     return total;
 }
 
 //better hash function
-
-f
+ 
+function hash(key, arrayLen){
+    let total = 0;
+    let WEIRD_PRIME = 31;
+    for(let i = 0; i < Math.min(key.length,100); i++){
+        let char = key[i];
+        let value = char.charCodeAt(0) -96;
+        total = (total + value + WEIRD_PRIME) % arrayLen;
+    }
+    return total;
+}
