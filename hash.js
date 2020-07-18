279b250f7f187315620148f07a1f64000cd7bae8
brian <brianbb174@protonmail.com>
2020-07-18 10:03:37 -0700

    initial hash

---
+++ hash.js
@@ -0,0 +1,16 @@
+
+//not so great hash
+
+function hash(key, arrayLen) {
+    let total = 0;
+    for (let i = 0; i < key.length; i++){
+        let char = key[i];
+        let value = char.charCodeAt(0) - 96;
+        total = (total + value) % arrayLen;
+    }
+    return total;
+}
+
+//better hash function
+
+f
